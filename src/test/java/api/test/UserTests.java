85546173b45393d433dd6680a111354211e17259
Parimal Jaldawar <parimaljaldawar@gmail.com>
2025-02-11 20:29:56 +0530

    Modified pom.xml file

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -1,13 +1,12 @@
 package api.test;
 
-import static org.testng.Assert.assertEquals;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.github.javafaker.Faker;
+
 import api.endpoints.UserEndpoints;
 import api.payload.User;
 import io.restassured.response.Response;
